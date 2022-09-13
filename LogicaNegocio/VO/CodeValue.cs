6d5f863444b80a2bec2f649cdf24a19563c89f8a
Sintesis <iribastrillo@gmail.com>
2022-09-12 21:39:27 -0300

    FEAT: implementa code value object y valida

--- LogicaNegocio/VO/CodeValue.cs
+++ LogicaNegocio/VO/CodeValue.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using LogicaNegocio.Excepciones;
 
 namespace LogicaNegocio.VO
 {
     public class CodeValue
     {
+        public List<char> validCodes = new List <char> () {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
         public string Value { get; private set; }
 
-        public CodeValue(string cod)
+        public CodeValue(string value)
         {
-            Validate(cod);
-            Value = cod;
+            Value = value;
+            validate();
         }
 
-        public void Validate(string cod)
+        public void validate()
         {
-            //TODO: Validar grupo.
+            foreach (var c in Value)
+            {
+                if (!validCodes.Contains(c))
+                {
+                    throw new InvalidCodeException("Invalid code: value must be character between A and H.");
+                }
+            }
         }
     
     }
