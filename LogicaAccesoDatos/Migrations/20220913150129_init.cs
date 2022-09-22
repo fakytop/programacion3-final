f2658896166f8ec6ac2956ec4c72481fa3f7bead
alumnoFI <alumnoFI@FILC007-16>
2022-09-21 21:37:26 -0300

    Agregar archivos de proyecto.

---
+++ LogicaAccesoDatos/Migrations/20220913150129_init.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LogicaAccesoDatos.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    ISOAlfa3 = table.Column<string>(nullable: true),
+                    GDP = table.Column<decimal>(type: "decimal(18,4)", nullable: true),
+                    Population = table.Column<int>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    Region = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
---
+++ LogicaNegocio/Interfaces/IRepositoryMatchResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using LogicaNegocio.Entidades;
+using System.Text;
+
+namespace LogicaNegocio.Interfaces
+{
+    public interface IRepositoryMatchResult: IRepository<MatchResult>
+    {
+    }
+}
