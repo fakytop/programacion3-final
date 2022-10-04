d38e9f4bef74a66f33206abfe6d6bae1c6498ffe
Gonzalo Perez Ramos <GP249454@fi365.ort.edu.uy>
2022-10-04 13:06:15 -0300

    controlador NT

---
+++ ApiApp/Controllers/NationalTeamsController.cs
@@ -0,0 +1,42 @@
+﻿using LogicaAplicacion.UseCases.Interfaces;
+using LogicaNegocio.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalTeamsController : ControllerBase
+    {
+        private ICreate<NationalTeam> _ucCreateNationalTeam;
+        private IRead<NationalTeam> _ucReadNationalTeam;
+        private IUpdate<NationalTeam> _ucUpdateNationalTeam;
+        private IDelete<NationalTeam> _ucDeleteNationalTeam;
+        private IRead<Country> _ucCountry;
+
+        public NationalTeamsController(
+            ICreate<NationalTeam> createNT,
+            IRead<NationalTeam> readNT,
+            IUpdate<NationalTeam> updateNT,
+            IDelete<NationalTeam> deleteNT,
+            IRead<Country> ucCountry
+            )
+        {
+            _ucCreateNationalTeam = createNT;
+            _ucReadNationalTeam = readNT;
+            _ucUpdateNationalTeam = updateNT;
+            _ucDeleteNationalTeam = deleteNT;
+            _ucCountry = ucCountry;
+        }
+
+        public IActionResult GetAll()
+        {
+            return Ok(_ucReadNationalTeam.ReadAll());
+        }
+    }
+}
