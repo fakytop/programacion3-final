82acbdb8d05c378e2f9051fc5e21afe64d5e79af
Gonzalo Perez Ramos <GP249454@fi365.ort.edu.uy>
2022-10-08 21:16:31 -0300

    allgun cambio

--- LogicaNegocio/VO/MatchDate.cs
+++ LogicaNegocio/VO/MatchDate.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LogicaNegocio.Excepciones;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,7 +8,7 @@ namespace LogicaNegocio.VO
     public class MatchDate
     {
         public DateTime Value { get; private set; }
-
+        public List<int> Horas = new List<int>{ 7, 10, 13, 16 };
         public MatchDate(DateTime value)
         {
             Value = value;
@@ -16,8 +17,17 @@ namespace LogicaNegocio.VO
 
         public void Validate()
         {
-            //TODO: validar.
-            //hola
+            DateTime comienzo = new DateTime(2022, 11, 20);
+            DateTime final = new DateTime(2022, 12, 2);
+
+            if(Value < comienzo || Value > final)
+            {
+                throw new DomainException("DateTime is wrong.");
+            }
+            if (!Horas.Contains(Value.Hour) || Value.Minute != 0 || Value.Second != 0)
+            {
+                throw new DomainException("Time isn't valid.");
+            }
         }
     }
 }
