528971c6b696e24ff2a8c87ae128e80af7a5ae88
Gonzalo Perez Ramos <GP249454@fi365.ort.edu.uy>
2022-11-09 20:32:12 -0300

    se agrega listado de paises al controlador api

---
+++ ApiApp/Controllers/CountriesController.cs
@@ -0,0 +1,37 @@
+﻿using ApiApp.Mapper;
+using LogicaAplicacion.UseCases.Interfaces;
+using LogicaNegocio.Entidades;
+using LogicaNegocio.Excepciones;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private IRead<Country> _ucReadCountry;
+
+        private IWebHostEnvironment _environment;
+
+        public CountriesController(
+            IRead<Country> ucReadCountries,
+            IWebHostEnvironment environment
+            )
+        {
+            _ucReadCountry = ucReadCountries;
+            _environment = environment;
+        }
+        public IActionResult GetAll()
+        {
+            return Ok(CountryMapper.FromCountries(_ucReadCountry.ReadAll()));
+        }
+
+    }
+}
